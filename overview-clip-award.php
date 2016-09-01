87f97bb5346a51ee65f9f42d7f0344b34dec7ab3
Hucius <huzi.michelle@gmail.com>
2016-09-01 12:29:25 +0800

    change unprofessional layout

--- overview-clip-award.php
+++ overview-clip-award.php
@@ -22,7 +22,7 @@
     <?php if (have_posts()) : while (have_posts()) : the_post();?>
       <div id="page-content">
         <div class="page">
-          <h2><?php the_title(); ?></h2>
+          <h2><?php the_title();?></h2>
           <?php the_content(); ?>
         </div>
       </div>
